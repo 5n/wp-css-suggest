732d0cb88c3e838501618d0ab599c915cd4ca07a
Genki Nakatani <genki.nakatani@gmail.com>
2021-04-21 00:11:11 +0900

    Create Tailwind Suggest and Enqueue style

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '2fa831f490c9d81a0462399873c81693');
