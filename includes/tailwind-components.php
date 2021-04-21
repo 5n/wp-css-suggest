2b16e5f4e6ac659fd143b06b6ec7687d8ac07d7b
Genki Nakatani <genki.nakatani@gmail.com>
2021-04-21 22:45:00 +0900

    Add Tailwind's background opacity

--- includes/tailwind-components.php
+++ includes/tailwind-components.php
@@ -886,6 +886,25 @@ function add_qtb_classes($classes) {
   // Background Color: https://tailwindcss.com/docs/background-color
   $tailwind['backgroundcolor'] = qtb_tailwind_color('bg');
 
+  // Background Opacity: https://tailwindcss.com/docs/background-opacity
+  $tailwind['backgroundopacity'] = [
+    'bg-opacity-0',
+    'bg-opacity-5',
+    'bg-opacity-10',
+    'bg-opacity-20',
+    'bg-opacity-25',
+    'bg-opacity-30',
+    'bg-opacity-40',
+    'bg-opacity-50',
+    'bg-opacity-60',
+    'bg-opacity-70',
+    'bg-opacity-75',
+    'bg-opacity-80',
+    'bg-opacity-90',
+    'bg-opacity-95',
+    'bg-opacity-100',
+  ];
+
   // Background Position: https://tailwindcss.com/docs/background-position
   $tailwind['backgroundposition'] = [
     'bg-bottom',
