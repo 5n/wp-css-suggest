acf7bb98a4834dd97a4243900b0ab834d0dbc092
Genki Nakatani <genki.nakatani@gmail.com>
2021-04-21 21:12:38 +0900

    Change filter priority

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '2fa831f490c9d81a0462399873c81693');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => '4a1bc64c1e697b69869ff155b954a454');
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /*
- * Tailwind className Suggestions
+ * CSS Class Suggest
  */
 
 import { __ } from "@wordpress/i18n";
-import { FormTokenField } from "@wordpress/components";
+import { BaseControl, FormTokenField } from "@wordpress/components";
 import { InspectorAdvancedControls } from "@wordpress/block-editor";
 import { hasBlockSupport } from "@wordpress/blocks";
 import { Fragment } from "@wordpress/element";
@@ -12,48 +12,51 @@ import { addFilter } from "@wordpress/hooks";
 import { useSelect } from "@wordpress/data";
 
 export const withInspectorControl = createHigherOrderComponent((BlockEdit) => {
-	return (props) => {
-		const hasCustomClassName = hasBlockSupport(
-			props.name,
-			"customClassName",
-			true
-		);
+  return (props) => {
+    const hasCustomClassName = hasBlockSupport(
+      props.name,
+      "customClassName",
+      true
+    );
 
-		if (hasCustomClassName && props.isSelected) {
-			const { className } = props.attributes;
+    if (hasCustomClassName && props.isSelected) {
+      const { className } = props.attributes;
 
-			const classNameArray = className ? className.trim().split(/\s+/) : [];
+      const classNameArray = className ? className.trim().split(/\s+/) : [];
 
-			const qtbSuggestions = useSelect(
-				(select) => select("core/editor").getEditorSettings().qtbSuggestions,
-				[]
-			);
+      const qtbSuggestions = useSelect(
+        (select) => select("core/editor").getEditorSettings().qtbSuggestions,
+        []
+      );
 
-			return (
-				<Fragment>
-					<BlockEdit {...props} />
-					<InspectorAdvancedControls>
-						<FormTokenField
-							label={__("Tailwind CSS class")}
-							value={classNameArray}
-							suggestions={qtbSuggestions}
-							onChange={(value) =>
-								props.setAttributes({
-									className: value.length ? value.join(" ") : undefined,
-								})
-							}
-						/>
-					</InspectorAdvancedControls>
-				</Fragment>
-			);
-		}
+      return (
+        <Fragment>
+          <BlockEdit {...props} />
+          <InspectorAdvancedControls>
+            <BaseControl className="qtb-classname-control">
+              <FormTokenField
+                label={__("CSS class suggest")}
+                value={classNameArray}
+                suggestions={qtbSuggestions}
+                onChange={(value) =>
+                  props.setAttributes({
+                    className: value.length ? value.join(" ") : undefined,
+                  })
+                }
+              />
+            </BaseControl>
+          </InspectorAdvancedControls>
+        </Fragment>
+      );
+    }
 
-		return <BlockEdit {...props} />;
-	};
+    return <BlockEdit {...props} />;
+  };
 }, "withInspectorControl");
 
 addFilter(
-	"editor.BlockEdit",
-	"plugin/qtb/with-inspector-control",
-	withInspectorControl
+  "editor.BlockEdit",
+  "plugin/qtb/with-inspector-control",
+  withInspectorControl,
+  9
 );
