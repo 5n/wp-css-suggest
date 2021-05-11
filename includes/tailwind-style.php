dc4b1771373f6439ccac06fc2245f825568c0e6b
Genki Nakatani <genki.nakatani@gmail.com>
2021-05-12 00:05:32 +0900

    Rename style handle

--- includes/tailwind-style.php
+++ includes/tailwind-style.php
@@ -4,7 +4,7 @@
  */
 
 function qtb_tailwind_scripts() {
-  wp_enqueue_style('tailwind-cdn-style', 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css');
+  wp_enqueue_style('cdn-tailwind', 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css');
 }
 add_action('wp_enqueue_scripts', 'qtb_tailwind_scripts');
 add_action('enqueue_block_editor_assets', 'qtb_tailwind_scripts');
